a6ec0f8eb42c5746e40033d159e29ccf1dc9397e
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 19:31:25 +0530

    init: transak-double-iframe

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <main className='container'>
+      {/* orange outer iframe */}
+      <iframe className="outer" src="https://27cb6826-7a95-4d9d-b07f-8eda8898e101-00-i2znckypxegk.worf.replit.dev"
+        allow="camera;microphone;payment"
+      />
+    </main>
+  )
+}
