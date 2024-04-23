8c94f287478923cce587b6f21193bd009f510a9f
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 19:46:21 +0530

    fix: supporter URL

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main className='container'>
       {/* orange outer iframe */}
-      <iframe className="outer" src="https://27cb6826-7a95-4d9d-b07f-8eda8898e101-00-i2znckypxegk.worf.replit.dev"
+      <iframe className="outer" src="https://transak-double-iframe-supporter.vercel.app/"
         allow="camera;microphone;payment"
       />
     </main>
