f4b97d141b3e1ac571b687264d37d95cde569333
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 19:59:01 +0530

    fix: configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import './App.css'
+import "./App.css";
+
+// type Environment = "STAGING" | "PRODUCTION";
 
 export default function App() {
+  // const [environment, setEnvironment] = useState<Environment>("STAGING")
 
   return (
-    <main className='container'>
+    <main className="container">
       {/* orange outer iframe */}
-      <iframe className="outer" src="https://transak-double-iframe-supporter.vercel.app/"
+      <iframe
+        className="outer"
+        src="https://transak-double-iframe-supporter.vercel.app/"
         allow="camera;microphone;payment"
       />
     </main>
-  )
+  );
 }
