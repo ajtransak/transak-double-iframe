27c8c27452a4be42a0ed38404725f19571d1ad00
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 23:24:25 +0530

    fix: add environments

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import "./App.css";
 
-// type Environment = "STAGING" | "PRODUCTION";
+type Environment = "STAGING" | "PRODUCTION";
 
 export default function App() {
-  // const [environment, setEnvironment] = useState<Environment>("STAGING")
+  const [environment, setEnvironment] = useState<Environment>("STAGING");
+
+  const toggleEnvironment = () => {
+    setEnvironment((prevEnvironment) =>
+      prevEnvironment === "STAGING" ? "PRODUCTION" : "STAGING"
+    );
+  };
 
   return (
     <main className="container">
-      {/* orange outer iframe */}
+      <span className="switch">
+        <label htmlFor="environmentToggle">
+          Current Environment: {environment}
+        </label>
+        <input
+          type="checkbox"
+          id="environmentToggle"
+          checked={environment === "PRODUCTION"}
+          onChange={toggleEnvironment}
+        />
+        <span className="slider round"></span>
+      </span>
       <iframe
         className="outer"
-        src="https://transak-double-iframe-supporter.vercel.app/"
+        src={
+          environment === "STAGING"
+            ? "https://transak-double-iframe-supporter.vercel.app/staging"
+            : "https://transak-double-iframe-supporter.vercel.app/production"
+        }
         allow="camera;microphone;payment"
       />
     </main>
